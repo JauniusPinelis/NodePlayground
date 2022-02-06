5bd96375254b42794ae7be0f592b733268954417
JauniusPinelis <JauniusPinelis@gmail.com>
2022-02-06 17:59:50 +0200

    comitting

--- microservices/posts/index.js
+++ microservices/posts/index.js
@@ -1,44 +1,45 @@
-const express = require('express');
-const cors = require('cors');
-const {randomBytes} = require('crypto');
-const axios = require('axios');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { randomBytes } = require("crypto");
+const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
-
-app.use(express.json());
+app.use(bodyParser.json());
 app.use(cors());
-
 
 const posts = {};
 
-app.get('/posts', (req,res) => {
-    res.send(posts);
+app.get("/posts", (req, res) => {
+  res.send(posts);
 });
 
-app.post('/posts', (req,res) => {
-    const id = randomBytes(4).toString('hex');
-    const {title} = req.body;
+app.post("/posts", async (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
 
-    posts[id] = { id, title};
+  posts[id] = {
+    id,
+    title,
+  };
 
-    axios.post('http://localhost:4005/events', {
-        type: 'PostCreated',
-        data: {
-            id,
-            title
-        }
-    }).catch((err) => {
-        console.log(err.message);
-    });
+  await axios.post("http://localhost:4005/events", {
+    type: "PostCreated",
+    data: {
+      id,
+      title,
+    },
+  });
 
-    res.status(201).send(posts[id]);
+  res.status(201).send(posts[id]);
 });
 
-app.post('/events', (req,res) => {
-    console.log('Received event', req.body.type);
-    res.send({});
+app.post("/events", (req, res) => {
+  console.log("Received Event", req.body.type);
+
+  res.send({});
 });
 
 app.listen(4000, () => {
-    console.log('Server started on port 4000');
-})
+  console.log("Listening on 4000");
+});
